2ae79cacbe34f67f1c2e6eb64d8615d1f264c435
tm-ahad <tm.ahad.07@gmail.com>
2024-02-13 03:31:27 +0600

    install.ps1 updated.

--- installations/install.ps1
+++ installations/install.ps1
@@ -1,1 +1,7 @@
+if (Test-Path $HOME\.lapis) 
+{
+    Remove-Item -Path "$HOME\.lapis" -Recurse -Force
+}
+
 git clone --depth 1 https://github.com/tm-ahad/lapis-deps.git $HOME\.lapis
+Remove-Item -Path "$HOME\.lapis\.git\" -Recurse -Force
