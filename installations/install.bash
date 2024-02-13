50ea9a64607fc38a67e6612806669d41534854b9
tm-ahad <tm.ahad.07@gmail.com>
2024-02-13 10:28:23 +0600

    install.bash updated.

--- installations/install.bash
+++ installations/install.bash
@@ -1,13 +1,20 @@
 #!/bin/bash
+LAPIS="$HOME/.lapis"
+LINUX = "$LAPIS/installations/bin/compiler/linux" 
 
-# Check if the OS is Linux
-if [[ "$(OSTYPE)" == "linux_gnu" ]]; then
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+
     if [[ -e "$HOME/.lapis" ]]; then
-        rmdir "$HOME/.lapis"
+        yes | rm -r "$HOME/.lapis"
     fi
 
-    git clone https://github.com/tm-ahad/lapis-deps.git $HOME/.lapis/
-    rm -r $HOME/.lapis/.git
+    git clone https://github.com/tm-ahad/lapis-deps.git $LAPIS
+    rm -f "$LAPIS/installations/bin/assembler/windows"
+    rm -f "$LAPIS/installations/bin/compiler/windows"
+
+    if [[ -f $LINUX ]]; then
+        mv -f $LINUX "/usr/bin/lapis"
+    fi
 
     echo "Lapis is successfully installed on your machine."
 else
--- installations/install.ps1
+++ installations/install.ps1
@@ -1,7 +1,13 @@
+$LAPIS = "$HOME\.lapis"
+
 if (Test-Path $HOME\.lapis) 
 {
-    Remove-Item -Path "$HOME\.lapis" -Recurse -Force
+    Remove-Item -Path $LAPIS-Recurse -Force
 }
 
 git clone --depth 1 https://github.com/tm-ahad/lapis-deps.git $HOME\.lapis
-Remove-Item -Path "$HOME\.lapis\.git\" -Recurse -Force
+Remove-Item -Path "$LAPIS\.git\" -Recurse -Force
+
+Remove-Item -Path "$LAPIS/installations/bin/assembler/linux" -Force
+Remove-Item -Path "$LAPIS/installations/bin/compiler/linux" -Force
+Rename-Item -Path "$LAPIS/installations/bin/compiler/windows" -NewName "lapis" -Force
